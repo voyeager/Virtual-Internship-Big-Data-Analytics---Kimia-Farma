89264d79635fde2e7afaae5368bf902455df54a4
voyeager <161406317+voyeager@users.noreply.github.com>
2024-04-01 00:58:32 +0700

    Update kimia_farma_script.sql

--- kimia_farma_script/kimia_farma_script.sql
+++ kimia_farma_script/kimia_farma_script.sql
@@ -17,33 +17,31 @@ CREATE ANALYTIC TABLE
   kc.branch_name,
   kc.kota,
   kc.provinsi,
-  kc.rating as rating_cabang,
+  kc.rating AS rating_cabang,
   ft.customer_name,
   ft.product_id,
   p.product_name,
-  p.price as actual_price,
-  ft.discount_percentage,
-  (SELECT 
+  p.price AS actual_price,
+  ft.discount_percentage, 
   CASE
     WHEN p.price <= 50000 THEN 0.10
     WHEN p.price BETWEEN 50000 AND 99999 THEN 0.15
     WHEN p.price BETWEEN 100000 AND 299999 THEN 0.20
     WHEN p.price BETWEEN 300000 AND 499999 THEN 0.25
     ELSE 0.30
-  END) as presentase_gross_laba,
-  (ft.price * ft.discount_percentage) as nett_sales,
-  ((ft.price * ft.discount_percentage) *
-  (SELECT 
-    CASE
+  END AS presentase_gross_laba,
+  ft.price * (1 - ft.discount_percentage) AS nett_sales,
+  (ft.price * (1 - ft.discount_percentage)) *
+  CASE
       WHEN p.price <= 50000 THEN 0.10
       WHEN p.price BETWEEN 50000 AND 99999 THEN 0.15
       WHEN p.price BETWEEN 100000 AND 299999 THEN 0.20
       WHEN p.price BETWEEN 300000 AND 499999 THEN 0.25
       ELSE 0.30
-    END)) as nett_profit,
+    END AS nett_profit,
   ft.rating as rating_transaksi
-FROM `kimia_farma.kf_final_transaction` as ft
-  LEFT JOIN `kimia_farma.kf_kantor_cabang` as kc
+FROM `kimia_farma.kf_final_transaction` AS ft
+  LEFT JOIN `kimia_farma.kf_kantor_cabang` AS kc
     ON ft.branch_id = kc.branch_id
-  LEFT JOIN `kimia_farma.kf_product` as p
+  LEFT JOIN `kimia_farma.kf_product` AS p
     ON ft.product_id = p.product_id;
